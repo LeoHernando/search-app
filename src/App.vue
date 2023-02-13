33b7b84a35a6da914a4d429e54b455839409b28e
Leo Hernando Tan <e0775385@u.nus.edu>
2023-02-13 22:46:44 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,12 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="p-14">
+    <span class="flex justify-center text-5xl text-blue-400"
+      >Pokemon Search&nbsp;<img
+        src="searchProject\search-app\src\assets\800px-Poké_Ball_icon.png"
+        class="w-10 h-10"
+    /></span>
+    <router-view />
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style></style>
