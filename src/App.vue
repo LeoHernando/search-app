d989bc0aaae7a0d188f022fc28db84b2317b9001
Leo Hernando Tan <e0775385@u.nus.edu>
2023-03-09 18:41:15 +0800

    fixed image issue

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div class="p-14">
     <span class="flex justify-center text-5xl text-blue-400"
       >Pokemon Search&nbsp;<img
-        src="searchProject\search-app\src\assets\800px-Poké_Ball_icon.png"
+        src="./assets/800px-Poké_Ball_icon.png"
         class="w-10 h-10"
     /></span>
     <router-view />
