f748805398c9f636a3bde79d2280b957d30f5ea4
Leo Hernando Tan <l.hernando.tan@gmail.com>
2023-04-27 20:40:32 +0800

    fixed merge conflicts

--- src/main.js
+++ src/main.js
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import './assets/tailwind.css'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "./assets/tailwind.css";
 
-createApp(App).use(router).mount('#app')
-=======
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import './assets/tailwind.css'
-
-createApp(App).use(router).mount('#app')
->>>>>>> master
+createApp(App).use(router).mount("#app");
